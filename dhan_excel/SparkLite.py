47ffd192a69d670ca56f4c7acc5ad9ada1b7073a
santhoshmurali <santhoshmurali@gmail.com>
2024-12-24 16:15:10 +0530

    Final Version

---
+++ dhan_excel/SparkLite.py
@@ -0,0 +1,248 @@
+from os import access
+import xlwings as xw
+from connect_to_dhan import Connection
+from dhanhq import dhanhq
+import numpy as np
+import pandas as pd
+from dhanhq import marketfeed
+import yaml
+import time
+
+
+
+""" configuring the workbook and getting the security info
+"""
+# Load Excel file
+excel_file = 'DhanTrading.xlsx'
+workbook = xw.Book(excel_file)
+TradeSheet = workbook.sheets['Trade']
+OptionsLookUp = workbook.sheets['OptionsLookUp'] 
+IndexLookup = workbook.sheets['IndexLookup']
+
+
+def option_age(x):  # This function will classify the options strike if it belongs to Current series, Next series or Far next.
+    if x == 1.0:
+        return 'C'  # Current series
+    elif x == 2.0:
+        return 'N'  # Next Series
+    else:
+        return 'F'  # Far next series
+
+def connect_to_dhan():
+    config_file_path = r".\\config\\config.yaml" #This has the API key and client id
+    with open(config_file_path,'r') as config:
+        api_config = yaml.safe_load(config)
+    clinet_id = api_config.get('api_config')[1]
+    access_token = api_config.get('api_config')[2]
+
+    # Establish connection to Dhan
+    try:
+        DhanConnector = Connection(clinet_id, access_token)
+        ConnectionObject = DhanConnector.connect_dhan()
+        dhan = ConnectionObject['conn']
+    except Exception as e :
+        raise ConnectionError(f"Can't connect {e}")  
+
+    return({"connection":dhan,
+            "client_id":clinet_id,
+            "access_token":access_token})    
+
+
+
+def initial_sheet_config():
+    connections__= connect_to_dhan() #returned as dictionary but accessed like a list
+    dhan = connections__['connection']
+    client_id = connections__['client_id']
+    access_token = connections__['access_token']
+    # Load the latest keys and scripts metadata automatically
+    security_list = dhan.fetch_security_list("compact")
+    #--------------------------------------------------------------------------------------------
+    # index
+    #--------------------------------------------------------------------------------------------
+    mcx_fut =  security_list[(security_list['SEM_EXM_EXCH_ID']=='MCX') & 
+                                (security_list['SM_SYMBOL_NAME']=='CRUDEOIL') & 
+                                (security_list['SEM_INSTRUMENT_NAME']=='FUTCOM')]
+    index = security_list[(security_list['SEM_INSTRUMENT_NAME'] == 'INDEX') & (
+        (security_list['SEM_TRADING_SYMBOL']=='BANKNIFTY') |
+        (security_list['SEM_TRADING_SYMBOL']=='NIFTY')) & (security_list['SEM_SEGMENT']=='I')]
+    filtered_df_index = pd.concat([mcx_fut,index])
+    filtered_df_index = filtered_df_index.copy()
+    filtered_df_index['SEM_TRADING_SYMBOL']= [x[0] for x in filtered_df_index.SEM_TRADING_SYMBOL.str.split('-')]
+
+    filtered_df_index['Rank'] = filtered_df_index.groupby('SEM_EXM_EXCH_ID')['SEM_EXPIRY_DATE'].rank(method='dense', ascending=True)
+    filtered_df_index = filtered_df_index[(filtered_df_index['Rank']==1.0) | (np.isnan(filtered_df_index['Rank']))]
+    IndexLookup.clear()
+    IndexLookup.range('A1').options(index=False).value = filtered_df_index[['SEM_TRADING_SYMBOL','SEM_SMST_SECURITY_ID']]
+    #--------------------------------------------------------------------------------------------
+    # Filter for only NIFTY and BANKNIFTY OPTIONS and MCX
+    #--------------------------------------------------------------------------------------------
+    NSE = security_list[(
+        (security_list['SEM_EXM_EXCH_ID'] == 'NSE') & 
+        (security_list['SEM_INSTRUMENT_NAME'] == 'OPTIDX') & 
+        ((security_list['SEM_TRADING_SYMBOL'].str.startswith('BANKNIFTY')) | 
+            (security_list['SEM_TRADING_SYMBOL'].str.startswith('NIFTY')))
+        &
+        (security_list['SEM_TRADING_SYMBOL'].str.startswith('NIFTYNXT50') == False)
+    )]    
+    MCX = security_list[(security_list['SEM_EXM_EXCH_ID'] == 'MCX') & 
+                        (security_list['SEM_INSTRUMENT_NAME'] == 'OPTFUT') & 
+                        (security_list['SM_SYMBOL_NAME'] == 'CRUDEOIL')]
+
+    filtered_df = pd.concat([NSE, MCX])
+    filtered_df = filtered_df.copy()  
+
+    # Below logic will help us to classify the Series type {C - Current, N - Next and F - Future}
+    filtered_df['SEM_EXPIRY_DATE_CUSTOM'] = np.nan
+    filtered_df['SEM_EXPIRY_DATE_CUSTOM'] = pd.to_datetime(filtered_df.SEM_EXPIRY_DATE)
+    filtered_df['SEM_UNDERLYING'] = [x[0] for x in filtered_df.SEM_CUSTOM_SYMBOL.str.split(' ')]
+    filtered_df['Rank'] = filtered_df.groupby('SEM_UNDERLYING')['SEM_EXPIRY_DATE_CUSTOM'].rank(method='dense', ascending=True)
+    filtered_df['Series'] = filtered_df['Rank'].apply(option_age)
+
+    filtered_df = filtered_df[filtered_df['Series'] == 'C'].copy(deep=True)
+    filtered_df['SEM_STRIKE_PRICE'] = filtered_df['SEM_STRIKE_PRICE'].astype(int)
+    filtered_df = filtered_df[['SEM_SMST_SECURITY_ID','SEM_UNDERLYING','SEM_OPTION_TYPE','SEM_STRIKE_PRICE']].copy(deep=True)
+    filtered_df['Series'] = filtered_df['SEM_UNDERLYING']+"_"+filtered_df['SEM_OPTION_TYPE']+"_"+filtered_df['SEM_STRIKE_PRICE'].astype(str)
+    OptionsLookUp.clear()
+    OptionsLookUp.range('A1').options(index=False).value = filtered_df[['Series','SEM_SMST_SECURITY_ID']]
+    return({
+        "connection":dhan,
+        "client_id":client_id,
+        "access_token" : access_token
+    })
+
+
+
+def refresh_instruments(change_instrument, dhan):
+  
+    if TradeSheet.range("INSTRUMENT").value == "CRUDEOIL":
+        market_feed = "MCX_COMM"
+        index_key = str(int(TradeSheet.range("IndexKey").value))
+    else:
+        market_feed = "IDX_I"
+        index_key = str(int(TradeSheet.range("IndexKey").value))
+    lp_dict =  dhan.ohlc_data(securities={market_feed:[int(index_key)]})       
+    lp = lp_dict['data']['data'][market_feed][index_key]['last_price']
+    TradeSheet.range("INDEX_LTP").value =  lp
+    if change_instrument:
+        time.sleep(2)
+    atm_key = str(int(TradeSheet.range("ATM_KEY").value))
+    itm_1_key = str(int(TradeSheet.range("ITM_ONE_KEY").value))
+    itm_2_key = str(int(TradeSheet.range("ITM_TWO_KEY").value))
+    itm_3_key = str(int(TradeSheet.range("ITM_THREE_KEY").value))
+    range_mapping = {
+        index_key : 'INDEX_LTP',
+        atm_key : 'ATM_LTP',
+        itm_1_key : 'ITM_ONE_LTP',
+        itm_2_key : 'ITM_TWO_LTP',
+        itm_3_key : 'ITM_THREE_LTP'
+    }
+    return(range_mapping)
+
+
+def prepare_instruments(instrument_keys):
+    instruments = []
+    if TradeSheet.range("INSTRUMENT").value == "CRUDEOIL":
+        market_feed = marketfeed.MCX
+    else:
+        market_feed = marketfeed.NSE_FNO
+
+    index_key = [(market_feed,instrument_keys[0],marketfeed.Ticker)]
+    options_key = [(market_feed,x,marketfeed.Ticker) for x in instrument_keys[1:] ]
+    
+    instruments = index_key + options_key
+    return(instruments)
+
+
+
+def run_feed(clientid,accesstoken,dhan):
+    version = "v2"
+    try:
+        instruments_all = refresh_instruments(True,dhan)  # Update instruments by calling subscription_management
+        instruments = list(instruments_all.keys())
+        
+        prepared_instruments = prepare_instruments(instruments)
+        security_to_cell = instruments_all
+        data = marketfeed.DhanFeed(clientid, accesstoken, prepared_instruments, version)
+        STRIKE_CHECK = TradeSheet.range('ATM_KEY').value
+        INDEX_KEY =  TradeSheet.range('IndexKey').value
+        while True:
+            #Trade Flag
+            refresh = TradeSheet.range("Refresh").value
+            if refresh:  
+                TradeSheet.range("Refresh").value = False
+                refresh_start_time = time.time()
+                instruments_all = refresh_instruments(False,dhan)  # Update instruments by calling subscription_management
+                instruments = list(instruments_all.keys())
+                prepared_instruments = prepare_instruments(instruments)
+                security_to_cell = instruments_all
+                data.disconnect()          
+                data = marketfeed.DhanFeed(clientid, accesstoken, prepared_instruments, version)
+                refresh_end_time = time.time()
+                pd.DataFrame({'Time':[f"{(refresh_end_time - refresh_start_time)*1000} milliseconds for refresh"]}).to_csv(r"D:\AlgoTrading\Books\Sucessful Algorithmic Trading\SucessfulAlgoTrading\dhan_excel\update.csv")
+                print("Subscription updated.")
+
+            if ( STRIKE_CHECK != TradeSheet.range('ATM_KEY').value ):
+                refresh_start_time = time.time()
+                instruments_all = refresh_instruments(False,dhan)  # Update instruments by calling subscription_management
+                instruments = list(instruments_all.keys())
+                prepared_instruments = prepare_instruments(instruments)
+                security_to_cell = instruments_all
+                data.disconnect()          
+                data = marketfeed.DhanFeed(clientid, accesstoken, prepared_instruments, version)
+                refresh_end_time = time.time()
+                pd.DataFrame({'Time':[f"{(refresh_end_time - refresh_start_time)*1000} milliseconds for refresh"]}).to_csv(r"D:\AlgoTrading\Books\Sucessful Algorithmic Trading\SucessfulAlgoTrading\dhan_excel\update.csv")
+                STRIKE_CHECK = TradeSheet.range('ATM_KEY').value
+                print("Subscription updated based on Strike Change.")           
+
+            if (INDEX_KEY != TradeSheet.range('IndexKey').value):
+                refresh_start_time = time.time()
+                instruments_all = refresh_instruments(True,dhan)  # Update instruments by calling subscription_management
+                instruments = list(instruments_all.keys())
+                prepared_instruments = prepare_instruments(instruments)
+                security_to_cell = instruments_all
+                data.disconnect()          
+                data = marketfeed.DhanFeed(clientid, accesstoken, prepared_instruments, version)
+                refresh_end_time = time.time()
+                pd.DataFrame({'Time':[f"{(refresh_end_time - refresh_start_time)*1000} milliseconds for refresh"]}).to_csv(r"D:\AlgoTrading\Books\Sucessful Algorithmic Trading\SucessfulAlgoTrading\dhan_excel\update.csv")
+                INDEX_KEY =  TradeSheet.range('IndexKey').value
+                print("Subscription updated based on Strike Change.") 
+            
+            data.run_forever()
+            response = data.get_data()
+           
+
+            if response['security_id'] == int(instruments[0]) and response['type'] == 'Ticker Data':
+               TradeSheet.range("INDEX_LTP").value = response['LTP']  # Update index ltp
+           
+           
+            if response['type'] == 'Ticker Data' and response['security_id'] != int(instruments[0]) :
+                security_id = str(response['security_id'])
+                cell_ref = security_to_cell[security_id]
+                TradeSheet.range(cell_ref).value = response['LTP']
+            print(response)
+
+
+                                                               
+    except Exception as e:
+         print(e)
+    finally:
+        # Close Connection if it was opened
+        try:
+            data.disconnect()
+        except:
+            pass
+    pass
+
+
+def main():
+    set_up_worksheet = initial_sheet_config()
+    print("Connected to Dhan Data!")
+    dhan = set_up_worksheet['connection']
+    cid = set_up_worksheet['client_id']
+    atoken = set_up_worksheet['access_token']
+    while True:
+        run_feed(cid,atoken,dhan)
+
+if __name__ == "__main__":
+    main()
+
---
+++ dhan_excel/order_mgmt lite v1.py
@@ -0,0 +1,273 @@
+from os import access
+import xlwings as xw
+from connect_to_dhan import Connection
+from dhanhq import dhanhq
+import numpy as np
+import pandas as pd
+import yaml
+import time
+
+
+""" configuring the workbook and getting the security info
+"""
+# Load Excel file
+excel_file = 'DhanTrading.xlsx'
+workbook = xw.Book(excel_file)
+TradeSheet = workbook.sheets['Trade']
+
+
+
+
+
+INPUT_PROP = "I"
+QUANTITY = "J" #Order Quantity for CE
+ORD_ACTIVATED = "K" #If Order is activated for CE
+LTP_ = "H"
+SCRIPT_KEY = "G"
+BUY_ORDER_ID = "M"
+BUY_ORDER_STATUS = "N"
+BUY_PRICE = "O"
+PROFIT_ORDER_ID = "P"
+PROFIT_ORDER_STATUS = "Q"
+PROFIT_PRICE = "R"
+PROFIT_TGT = "S"
+SL_ORDER_ID = "T"
+SL_ORDER_STATUS = "U"
+SL_PRICE = "V"
+SL_TGT = "W"
+PROFIT_TRIGGER_PRICE = "X"
+SL_TRIGGER_PRICE = "Y"
+
+
+
+
+def connect_to_dhan():
+    config_file_path = r".\\config\\config.yaml" #This has the API key and client id
+    with open(config_file_path,'r') as config:
+        api_config = yaml.safe_load(config)
+    clinet_id = api_config.get('api_config')[1]
+    access_token = api_config.get('api_config')[2]
+
+    # Establish connection to Dhan
+    try:
+        DhanConnector = Connection(clinet_id, access_token)
+        ConnectionObject = DhanConnector.connect_dhan()
+        dhan = ConnectionObject['conn']
+    except Exception as e :
+        raise ConnectionError(f"Can't connect {e}")  
+
+    return({"connection":dhan,
+            "client_id":clinet_id,
+            "access_token":access_token})    
+
+#Order Placement Functions
+def place_buy_order(dhan,script_key, order_qty, trade_price,correlation_id):
+    if TradeSheet.range("INSTRUMENT").value == "CRUDEOIL":
+        segment = dhanhq.MCX
+    else:
+        segment = dhanhq.NSE_FNO
+    SLICE_ORDER_BUY = dhan.place_order(security_id=str(int(script_key)), exchange_segment=segment, transaction_type=dhanhq.BUY, quantity=str(int(order_qty)),
+                           order_type=dhanhq.LIMIT, product_type=dhanhq.INTRA, price=trade_price, trigger_price= trade_price-0.1, disclosed_quantity=0,
+                           after_market_order=False, validity='DAY', amo_time='OPEN',
+                           bo_profit_value=None, bo_stop_loss_Value=None, tag="ALGOENTRY") 
+    print(SLICE_ORDER_BUY)
+    return(SLICE_ORDER_BUY)
+
+
+#Place Profit Order
+def place_profit_order(dhan,script_key, order_qty, trade_price,correlation_id):
+    if TradeSheet.range("INSTRUMENT").value == "CRUDEOIL":
+        segment = dhanhq.MCX
+    else:
+        segment = dhanhq.NSE_FNO    
+    SLICE_ORDER_SELL_PROFIT = dhan.place_order(security_id=str(int(script_key)), exchange_segment=segment, transaction_type=dhanhq.SELL, quantity=int(order_qty),
+                           order_type=dhanhq.LIMIT, product_type=dhanhq.INTRA, price=np.round(float(trade_price),1),disclosed_quantity=0, validity='DAY',amo_time='OPEN',
+                            tag="ALGOPFT")
+    print(SLICE_ORDER_SELL_PROFIT)
+    return(SLICE_ORDER_SELL_PROFIT)
+
+#Place SL Order
+def place_sl_order(dhan,script_key, order_qty, trade_price,correlation_id):
+    if TradeSheet.range("INSTRUMENT").value == "CRUDEOIL":
+        segment = dhanhq.MCX
+    else:
+        segment = dhanhq.NSE_FNO
+    SLICE_ORDER_SELL_LOSS = dhan.place_order(security_id=str(int(script_key)), exchange_segment=segment, transaction_type=dhanhq.SELL, quantity=int(order_qty),
+                           order_type=dhanhq.SL, product_type=dhanhq.INTRA, price=np.round(float(trade_price),1), trigger_price=np.round(float(trade_price),1)+0.2, disclosed_quantity=0, 
+                           validity='DAY', amo_time='OPEN',tag="ALGOLOSS")
+    print(SLICE_ORDER_SELL_LOSS)
+    return(SLICE_ORDER_SELL_LOSS)
+
+def place_cancel_order(dhan,orderid):
+    CANCEL_ORDER = dhan.cancel_order(orderid)
+    return(CANCEL_ORDER)
+
+
+#Order Management
+def get_order_details(dhan):
+    # Buy Order Object
+    BUY_ORDER = [None for k in range(4)]
+
+    # Sell Profit Object
+    PROFIT_ORDER = [None for k in range(4)]
+
+    # Sell Loss Object
+    LOSS_ORDER = [None for k in range(4)]
+
+    TRADE_COMPLETE = False  
+    SWING_SWITCH = False
+
+    
+    TEMP_TGT_PROFIT = None
+    TEMP_TGT_SL = None
+
+    while True:
+        for strikes in range(4):
+
+            # # NIFTY CE 
+            # Stage 1 : Check if the User initatd any orders, if yes. Place the Buy order and do not execute this if again. This is controlled using Activated flag in Excel mapped using  ce_col_activated
+            if (TradeSheet.range(f'{QUANTITY}{strikes+6}').value and TradeSheet.range(f'{ORD_ACTIVATED}{strikes+6}').value == False and TRADE_COMPLETE == False): 
+                Quantity = TradeSheet.range(f'{QUANTITY}{strikes+6}').value
+                Script_Key = TradeSheet.range(f'{SCRIPT_KEY}{strikes+6}').value
+                ltp__ = TradeSheet.range(f'{LTP_}{strikes+6}').value
+                buy_order  = place_buy_order(dhan,Script_Key,Quantity,ltp__-0.1,"ALGO_ORDER")
+                print(buy_order)
+                BUY_ORDER[strikes] = buy_order
+                TradeSheet.range(f'{ORD_ACTIVATED}{strikes+6}').value = True
+                TradeSheet.range(f'{BUY_ORDER_ID}{strikes+6}').value = None
+                TradeSheet.range(f'{BUY_ORDER_STATUS}{strikes+6}').value = None
+                TradeSheet.range(f'{BUY_PRICE}{strikes+6}').value = None
+                TradeSheet.range(f'{PROFIT_ORDER_ID}{strikes+6}').value = None
+                TradeSheet.range(f'{PROFIT_ORDER_STATUS}{strikes+6}').value = None
+                TradeSheet.range(f'{PROFIT_PRICE}{strikes+6}').value = None
+                TradeSheet.range(f'{SL_ORDER_ID}{strikes+6}').value = None
+                TradeSheet.range(f'{SL_ORDER_STATUS}{strikes+6}').value = None
+                TradeSheet.range(f'{SL_ORDER_STATUS}{strikes+6}').value = None
+                TradeSheet.range(f'{SL_PRICE}{strikes+6}').value = None
+                
+            
+            # Stage 2 : If the order is placed successfully, we have to start capturing the status of the order as the initial status and leave this condition.
+            if (TradeSheet.range(f'{QUANTITY}{strikes+6}').value and TradeSheet.range(f'{ORD_ACTIVATED}{strikes+6}').value == True and BUY_ORDER[strikes]
+                and not(TradeSheet.range(f'{BUY_ORDER_ID}{strikes+6}').value)):
+                
+                ord_id = BUY_ORDER[strikes]['data']['orderId']
+                order_details = dhan.get_order_by_id(ord_id)['data'][0]
+                TradeSheet.range(f'{BUY_ORDER_ID}{strikes+6}').value  = order_details['orderId']
+                TradeSheet.range(f'{BUY_ORDER_STATUS}{strikes+6}').value  = order_details['orderStatus']
+
+            # Stage 3 : Following condition will update respective cells in the Excel until the Status is changed to "TRADED"
+            if (TradeSheet.range(f'{QUANTITY}{strikes+6}').value and TradeSheet.range(f'{ORD_ACTIVATED}{strikes+6}').value == True and 
+                TradeSheet.range(f'{BUY_ORDER_ID}{strikes+6}').value and TradeSheet.range(f'{BUY_ORDER_STATUS}{strikes+6}').value != "TRADED"):
+                ord_id = BUY_ORDER[strikes]['data']['orderId']
+                order_details = dhan.get_order_by_id(ord_id)['data'][0]
+                TradeSheet.range(f'{BUY_ORDER_ID}{strikes+6}').value  = order_details['orderId']
+                TradeSheet.range(f'{BUY_ORDER_STATUS}{strikes+6}').value  = order_details['orderStatus']    
+            
+            # Stage 4 : This is where all drama happens. Once the status is updated to "TRADED", we begin Exit Phase.
+            if (TradeSheet.range(f'{QUANTITY}{strikes+6}').value and TradeSheet.range(f'{ORD_ACTIVATED}{strikes+6}').value == True and 
+                TradeSheet.range(f'{BUY_ORDER_ID}{strikes+6}') and TradeSheet.range(f'{BUY_ORDER_STATUS}{strikes+6}').value == "TRADED"): 
+                Script_Key = TradeSheet.range(f'{SCRIPT_KEY}{strikes+6}').value
+                avg_executed_price =  order_details['price']
+                TradeSheet.range(f'{BUY_PRICE}{strikes+6}').value = avg_executed_price
+                ltp_ = TradeSheet.range(f'{LTP_}{strikes+6}').value
+                trigger_profit_ = TradeSheet.range(f'{PROFIT_TRIGGER_PRICE}{strikes+6}').value
+                trigger_sl_ = TradeSheet.range(f'{SL_TRIGGER_PRICE}{strikes+6}').value
+                profit_price_ = TradeSheet.range(f'{PROFIT_TGT}{strikes+6}').value
+                sl_price_ = TradeSheet.range(f'{SL_TGT}{strikes+6}').value
+                Quantity = TradeSheet.range(f'{QUANTITY}{strikes+6}').value
+
+                if ltp_<trigger_profit_ and ltp_ > trigger_sl_:
+                    TEMP_TGT_SL = None
+                    TEMP_TGT_PROFIT = None
+                    SWING_SWITCH = False
+                elif ltp_>= trigger_profit_:
+                    TEMP_TGT_PROFIT = trigger_profit_
+                    TEMP_TGT_SL = None
+                elif ltp_ <= trigger_sl_:
+                    TEMP_TGT_SL = trigger_sl_
+                    TEMP_TGT_PROFIT = None
+                
+                
+
+
+                # Logic for handling the ordes when price is swining between Tgt pl and tgt sl
+                # If LTP moves above trigger profit we turn the switch On and place the profit order                    
+                if ltp_>=trigger_profit_ and not(PROFIT_ORDER[strikes]) and not(SWING_SWITCH):
+                        PROFIT_ORDER[strikes] = place_profit_order(dhan,Script_Key,Quantity,profit_price_,"")
+                        if (LOSS_ORDER[strikes]):
+                            CANCEL_ORD = place_cancel_order(dhan,str(LOSS_ORDER[strikes]['data']['orderId']))
+                            LOSS_ORDER[strikes] = None
+                            TradeSheet.range(f'{SL_ORDER_ID}{strikes+6}').value = CANCEL_ORD['data']['orderId']
+                            TradeSheet.range(f'{SL_ORDER_STATUS}{strikes+6}').value = CANCEL_ORD['status']
+                        SWING_SWITCH = True
+                # We Monitor the order till it is not TRADED and SWITCH is ON
+                if (PROFIT_ORDER[strikes]  and PROFIT_ORDER[strikes]['status']=='success' and SWING_SWITCH):
+                    ord_id_p = PROFIT_ORDER[strikes]['data']['orderId']
+                    order_details_p = dhan.get_order_by_id(ord_id_p)['data'][0]
+                    TradeSheet.range(f'{SL_ORDER_ID}{strikes+6}').value = None
+                    TradeSheet.range(f'{SL_ORDER_STATUS}{strikes+6}').value = None                   
+                    TradeSheet.range(f'{PROFIT_ORDER_ID}{strikes+6}').value = ord_id_p
+                    TradeSheet.range(f'{PROFIT_ORDER_STATUS}{strikes+6}').value = order_details_p['orderStatus']
+                    print(f'Profit Order Status {order_details_p['orderStatus']}')
+                  # Profit Order is succes    
+                if(PROFIT_ORDER[strikes] and TradeSheet.range(f'{PROFIT_ORDER_STATUS}{strikes+6}').value == 'TRADED' and SWING_SWITCH):
+                    SWING_SWITCH = False
+                    PROFIT_ORDER[strikes] = None
+                    TRADE_COMPLETE = True
+
+                # If LTP moves below trigger SL we turn the switch On and place the loss order
+                if ltp_<=trigger_sl_ and not(LOSS_ORDER[strikes]) and not(SWING_SWITCH):
+                        LOSS_ORDER[strikes] = place_sl_order(dhan,Script_Key,Quantity,sl_price_,"")
+                        if (PROFIT_ORDER[strikes]):
+                            CANCEL_ORD = place_cancel_order(dhan,str(PROFIT_ORDER[strikes]['data']['orderId']))
+                            PROFIT_ORDER[strikes] = None
+                            TradeSheet.range(f'{PROFIT_ORDER_ID}{strikes+6}').value =  CANCEL_ORD['data']['orderId']
+                            TradeSheet.range(f'{PROFIT_ORDER_STATUS}{strikes+6}').value = CANCEL_ORD['status']                        
+                        SWING_SWITCH = True           
+                # We Monitor the order till it is not TRADED and SWITCH is ON
+                if(LOSS_ORDER[strikes] and LOSS_ORDER[strikes]['status']=='success' and SWING_SWITCH):
+                    ord_id_l = LOSS_ORDER[strikes]['data']['orderId']
+                    order_details_l = dhan.get_order_by_id(ord_id_l)['data'][0]
+                    TradeSheet.range(f'{PROFIT_ORDER_ID}{strikes+6}').value = None
+                    TradeSheet.range(f'{PROFIT_ORDER_STATUS}{strikes+6}').value = None                  
+                    TradeSheet.range(f'{SL_ORDER_ID}{strikes+6}').value = ord_id_l
+                    TradeSheet.range(f'{SL_ORDER_STATUS}{strikes+6}').value = order_details_l['orderStatus']
+                    print(f'SL Order Status {order_details_l['orderStatus']}')
+                # Loss Order is succes    
+                if(LOSS_ORDER[strikes] and TradeSheet.range(f'{SL_ORDER_STATUS}{strikes+6}').value == 'TRADED' and SWING_SWITCH ):
+                    SWING_SWITCH = False
+                    LOSS_ORDER[strikes] = None
+                    TRADE_COMPLETE = True                                          
+                
+                
+                if TRADE_COMPLETE:
+                    TradeSheet.range(f'{INPUT_PROP}{strikes+6}').value = None
+                    TradeSheet.range(f'{ORD_ACTIVATED}{strikes+6}').value = False                    
+                    TradeSheet.range(f'{BUY_ORDER_ID}{strikes+6}').value = None
+                    TradeSheet.range(f'{BUY_ORDER_STATUS}{strikes+6}').value = None
+                    TradeSheet.range(f'{BUY_PRICE}{strikes+6}').value = None
+                    TradeSheet.range(f'{PROFIT_ORDER_ID}{strikes+6}').value = None
+                    TradeSheet.range(f'{PROFIT_ORDER_STATUS}{strikes+6}').value = None
+                    TradeSheet.range(f'{PROFIT_PRICE}{strikes+6}').value = None
+                    TradeSheet.range(f'{SL_ORDER_ID}{strikes+6}').value = None
+                    TradeSheet.range(f'{SL_ORDER_STATUS}{strikes+6}').value = None
+                    TradeSheet.range(f'{SL_ORDER_STATUS}{strikes+6}').value = None
+                    TradeSheet.range(f'{SL_PRICE}{strikes+6}').value = None 
+                    TRADE_COMPLETE = False
+
+            # elif (not(TradeSheet.range(f'{QUANTITY}{strikes+6}').value)):
+                # TradeSheet.range(f'{ce_col_activated}{strikes+6}').value = False
+            
+
+            
+
+def main():
+    connections__= connect_to_dhan() #returned as dictionary but accessed like a list
+    dhan = connections__['connection']
+    # print(instruments)
+    print("Order MGMT started!")
+    get_order_details(dhan)
+
+
+if __name__ == "__main__":
+    main()
